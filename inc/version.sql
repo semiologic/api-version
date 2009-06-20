02b398e5885755252158926f5c79d94d33e784ce
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-06-20 11:41:57 +0000

    fix sample files
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/api/version/trunk@1795 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/version.sql
+++ inc/version.sql
@@ -20,7 +20,7 @@ INSERT INTO packages ( type, slug, url, stable_version, stable_package, bleeding
 VALUES
 	( 'core', 'sem-pro', 'http://www.getsemiologic.com',
 		'5.7.1', 'http://www.semiologic.com/members/sem-pro/download/sem-pro.zip',
-		'6.0 beta', 'http://www.semiologic.com/members/sem-pro/bleeding/sem-pro-dev.zip'
+		'6.0 beta', 'http://www.semiologic.com/media/members/sem-pro/bleeding/sem-pro-dev.zip'
 	),
 	( 'themes', 'sem-reloaded', 'http://www.semiologic.com/software/sem-reloaded/',
 		'0.5', 'http://downloads.wordpress.org/themes/sem-reloaded.zip',
@@ -28,9 +28,9 @@ VALUES
 	),
 	( 'plugins', 'ad-manager', 'http://www.semiologic.com/software/ad-manager/',
 		'1.2', 'http://www.semiologic.com/members/plugins/ad-manager/ad-manager.zip',
-		'2.0 RC2', 'http://www.semiologic.com/members/plugins/ad-manager/ad-manager-dev.zip'
+		'2.0 RC2', 'http://www.semiologic.com/media/members/plugins/ad-manager/ad-manager-dev.zip'
 	),
 	( 'plugins', 'version-checker', 'http://www.semiologic.com/software/version-checker/',
 		'2.0', 'http://www.semiologic.com/members/plugins/version-checker/version-checker.zip',
-		'2.0', 'http://www.semiologic.com/members/plugins/version-checker/version-checker-dev.zip'
+		'2.0', 'http://www.semiologic.com/media/members/plugins/version-checker/version-checker-dev.zip'
 	);
