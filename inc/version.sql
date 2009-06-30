bf6e11141fa15888968fe7cd60d206657fe39928
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-06-30 20:26:29 +0000

    dump the corrupt test data
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/api/version/trunk@1889 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/version.sql
+++ inc/version.sql
@@ -15,22 +15,3 @@ CREATE TABLE IF NOT EXISTS packages (
 	PRIMARY KEY ( id ),
 	UNIQUE KEY ( type, slug )
 ) DEFAULT CHARSET=UTF8;
-
-INSERT INTO packages ( type, slug, url, stable_version, stable_package, bleeding_version, bleeding_package )
-VALUES
-	( 'core', 'sem-pro', 'http://www.getsemiologic.com',
-		'5.7.1', 'http://www.semiologic.com/members/sem-pro/download/sem-pro.zip',
-		'6.0 beta', 'http://www.semiologic.com/media/members/sem-pro/bleeding/sem-pro-dev.zip'
-	),
-	( 'themes', 'sem-reloaded', 'http://www.semiologic.com/software/sem-reloaded/',
-		'0.5', 'http://downloads.wordpress.org/themes/sem-reloaded.zip',
-		'2.0 RC2', 'http://www.semiologic.com/media/software/sem-reloaded/sem-reloaded-dev.zip'
-	),
-	( 'plugins', 'ad-manager', 'http://www.semiologic.com/software/ad-manager/',
-		'1.2', 'http://www.semiologic.com/members/plugins/ad-manager/ad-manager.zip',
-		'2.0 RC2', 'http://www.semiologic.com/media/members/plugins/ad-manager/ad-manager-dev.zip'
-	),
-	( 'plugins', 'version-checker', 'http://www.semiologic.com/software/version-checker/',
-		'2.0', 'http://www.semiologic.com/members/plugins/version-checker/version-checker.zip',
-		'2.0', 'http://www.semiologic.com/media/members/plugins/version-checker/version-checker-dev.zip'
-	);
