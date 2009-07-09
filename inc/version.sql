e16e9a047afb06d5904ea65c240f391e1136b903
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-09 23:24:30 +0000

    schema tweaks
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/api/version/trunk@2032 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/version.sql
+++ inc/version.sql
@@ -3,15 +3,15 @@ CREATE TABLE IF NOT EXISTS packages (
 	type				varchar(64),
 	slug				varchar(64),
 	url					varchar(255),
-	readme				text NOT NULL DEFAULT '',
 	stable_version		varchar(32) NOT NULL DEFAULT '',
 	stable_requires		varchar(32) NOT NULL DEFAULT '',
 	stable_compat		varchar(32) NOT NULL DEFAULT '',
 	stable_package		varchar(255),
 	bleeding_version	varchar(32) NOT NULL DEFAULT '',
-	bleeding_package	varchar(255),
 	bleeding_requires	varchar(32) NOT NULL DEFAULT '',
 	bleeding_compat		varchar(32) NOT NULL DEFAULT '',
+        bleeding_package        varchar(255),
+        readme                          text NOT NULL DEFAULT '',
 	PRIMARY KEY ( id ),
 	UNIQUE KEY ( type, slug )
 ) DEFAULT CHARSET=UTF8;
